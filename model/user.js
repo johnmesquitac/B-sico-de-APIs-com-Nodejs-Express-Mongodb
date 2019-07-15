48cc4b4098b8edb340a374815f9844801d3d394f
johnmesquitac <mesquitajoaoc@outlook.com>
2019-07-15 20:16:08 -0300

    Aula 5 - Criptografia com Bcrypt

--- model/user.js
+++ model/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt');
 //criando um schema, ou seja, uma tabela de usuários
 const UserSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true},
@@ -8,4 +8,15 @@ const UserSchema = new Schema({
     created: { type: Date, default: Date.now}
 })
 
+/* modulo presave faz com que a senha do usuário seja criptografada ao armazenar no banco*/
+
+UserSchema.pre('save', function(next){
+    let user = this;
+    if(!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, 10, (err, encrypted)=>{
+        user.password = encrypted;
+        return next();
+    })
+});
 module.exports = mongoose.model('User', UserSchema);
