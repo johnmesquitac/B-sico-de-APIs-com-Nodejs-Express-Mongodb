ef130c422e0ec5b1f5d700119bb793ba18419547
johnmesquitac <mesquitajoaoc@outlook.com>
2019-07-16 12:58:51 -0300

    Aula 9 - Retornando Status Code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 const auth = (req, res, next)=>{
     const token_header = req.headers.auth;
 
-    if(!token_header) return res.send({error: 'Authentication Denied!'});
+    if(!token_header) return res.status(401).send({error: 'Authentication Denied!'});
 
     jwt.verify(token_header,'akira',(err,decoded)=>{
-        if(err) return res.send({error: 'Invalid Token!'});
+        if(err) return res.status(401).send({error: 'Invalid Token!'});
         res.locals.auth_data = decoded;
         return next();
     })
